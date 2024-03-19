6340fc4e8138e475e5033a57a4e5bb71dc86f441
mo'men <momenali5792@gmail.com>
2024-03-19 22:26:35 +0200

    "Task 3 files were commited"

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -18,88 +18,90 @@ class LoginScreen extends StatelessWidget {
         backgroundColor: const Color(0xff2196F3),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            const SizedBox(
-              height: 30,
-            ),
-            const Text('Codeplayon',
-            style:
-              TextStyle(
-                fontSize: 40,
-                color:  Color(0xff2196F3),
-                fontWeight: FontWeight.w500,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 30,
               ),
-            ),
-            const SizedBox(
-              height: 12,
-            ),
-            const TextFieldWidget(text: 'User Name'),
-            const TextFieldWidget(text: 'Password'),
-            const SizedBox(
-              height: 3,
-            ),
-            const Text('Forgot Password',
+              const Text('Codeplayon',
               style:
-              TextStyle(
-                fontSize: 18,
-                color:  Color(0xff2196F3),
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 15),
-              child: MaterialButton(
-                height: 58.0,
-                minWidth: double.maxFinite,
-                color:const Color(0xff2196F3),
-                textColor: Colors.white,
-                onPressed: () => {},
-                child: const Text("Login",
-                style: TextStyle(
-                  fontSize: 16,
-                ),
+                TextStyle(
+                  fontSize: 40,
+                  color:  Color(0xff2196F3),
+                  fontWeight: FontWeight.w500,
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            const Row(
-              children: [
-                Spacer(
-                  flex: 6,
+              const SizedBox(
+                height: 12,
+              ),
+              const TextFieldWidget(text: 'User Name'),
+              const TextFieldWidget(text: 'Password'),
+              const SizedBox(
+                height: 3,
+              ),
+              const Text('Forgot Password',
+                style:
+                TextStyle(
+                  fontSize: 18,
+                  color:  Color(0xff2196F3),
+                  fontWeight: FontWeight.w500,
                 ),
-                Text('Does not have account?',
-                  style:
-                  TextStyle(
-                    fontSize: 17,
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: MaterialButton(
+                  height: 58.0,
+                  minWidth: double.maxFinite,
+                  color:const Color(0xff2196F3),
+                  textColor: Colors.white,
+                  onPressed: () => {},
+                  child: const Text("Login",
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
                   ),
                 ),
-                Spacer(
-                  flex: 1,
-                ),
-                Text('Sign up',
-                  style:
-                  TextStyle(
-                    fontSize: 22,
-                    color:  Color(0xff2196F3),
-                    fontWeight: FontWeight.w500,
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              const Row(
+                children: [
+                  Spacer(
+                    flex: 6,
                   ),
-                ),
-                Spacer(
-                  flex: 6,
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-          ],
+                  Text('Does not have account?',
+                    style:
+                    TextStyle(
+                      fontSize: 17,
+                    ),
+                  ),
+                  Spacer(
+                    flex: 1,
+                  ),
+                  Text('Sign up',
+                    style:
+                    TextStyle(
+                      fontSize: 22,
+                      color:  Color(0xff2196F3),
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  Spacer(
+                    flex: 6,
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+            ],
+          ),
         ),
       ),
     );
